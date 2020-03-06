3a1132d47b9cebc4c01e4bac3962c169ac68498f
binding-archive-7591 <yarhumher@hotmail.com>
2020-03-06 03:15:05 +0000

    Done.

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -18,12 +18,16 @@ def second_supply_for_fourth_of_july(holiday_hash)
   #   }
   # }
   # return the second element in the 4th of July array
+  holiday_hash[:summer][:fourth_of_july][1]
+
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
   # holiday_hash is identical to the one above
   # add the second argument, which is a supply, to BOTH the
   # Christmas AND the New Year's arrays
+holiday_hash[:winter][:christmas] << supply
+holiday_hash[:winter][:new_years] << supply
 
 end
 
@@ -31,21 +35,45 @@ end
 def add_supply_to_memorial_day(holiday_hash, supply)
   # again, holiday_hash is the same as the ones above
   # add the second argument to the memorial day array
+  holiday_hash[:spring][:memorial_day] << supply
 
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
   # code here
   # remember to return the updated hash
+  # columbus_day_supplies = ["Flags", "Parade Floats", "Italian Food"]
+  # add_new_holiday_with_supplies(holiday_supplies, :fall, :columbus_day, columbus_day_supplies)
+  columbus = holiday_hash[season][holiday_name] = supply_array
+
+
 
 end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
 
+holiday_hash[:winter][:christmas] = ["Lights", "Wreath", "Party Hats"]
+holiday_hash[:winter][:new_years] = ["Lights", "Wreath", "Party Hats"]
 end
 
 def all_supplies_in_holidays(holiday_hash)
+  holiday_hash.map {|key,value|
+          stations = key.to_s << ":"
+          capitalized_word = stations.capitalize!
+          puts capitalized_word
+          value.map{|stations_keys|
+              stations_keys[0] = stations_keys[0].to_s << ":"
+              new_array = []
+              stations_keys[0].split("_").map {|capitalize|
+                  new_array.push(capitalize.capitalize!)
+              }
+          holidays_string = new_array.join(" ")
+          string = stations_keys[1].join(", ")
+          puts "  #{holidays_string} #{string}"
+          string = stations_keys[1].join(", ")
+          }
+      }
   # iterate through holiday_hash and print items such that your readout resembles:
   # Winter:
   #   Christmas: Lights, Wreath
@@ -53,18 +81,15 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-
 end
 
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-
+  holiday = []
+     summer = holiday_hash[:summer].keys[0]
+     spring =  holiday_hash[:spring].keys[0]
+     holiday.push(summer)
+     holiday.push(spring)
+     holiday
 end
-
-
-
-
-
-
-
