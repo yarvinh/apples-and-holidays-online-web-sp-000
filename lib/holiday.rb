196ced91dd97cc787780490307c0398aa5ff8c70
binding-archive-7591 <yarhumher@hotmail.com>
2020-03-06 02:47:17 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -87,7 +87,7 @@ def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
   holiday_hash.map {|station, station_values|
-    station_values.size
+    station_values.key
   }
 
 end
